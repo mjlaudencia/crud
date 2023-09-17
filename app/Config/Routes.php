f734ef766b7eb8da05c186cde417db6187003293
mjlaudencia <laudenciamj3@gmail.com>
2023-09-18 04:24:20 +0800

    views

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('', 'Home::index');
-$routes->get('/mjlaudencia', 'Home::mjlaudencia');
+$routes->get('/product', 'ProductController::mjlaudencia');
+$routes->get('/product/(:any)', 'ProductController::product/$1');
--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,6 +6,15 @@ use App\Controllers\BaseController;
 
 class ProductController extends BaseController
 {
+    public function product($product)
+    {
+        echo $product;
+    }
+    
+    public function mjlaudencia()
+    {
+        echo 'working';
+    }
     public function index()
     {
         //
